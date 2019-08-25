9640e19a1534c4c1a1f0531a10b6cc251efa6542
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-08-26 01:01:07 +0300

    Create Identifier.js

---
+++ week 2/Identifier.js
@@ -0,0 +1,6 @@
+function first_months(){ // first_months is the identifier which identifies the function's name 
+  months_names = ["Januray","February","March","April","May","June"]; // months_names is the Identifier
+  document.write(months_names);
+}
+// call the function 
+first_months(); 
