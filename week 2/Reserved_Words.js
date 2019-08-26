8ce0f4373eeeecb6314d9b9e4328f6c7f9ca9257
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-08-27 00:22:12 +0300

    Create Reserved_Words.js

---
+++ week 2/Reserved_Words.js
@@ -0,0 +1,3 @@
+
+var Reserved_Words= ["break","const","let","abstact","Window"];
+document.write("Reserved Words are words that are taken and can not be used as identifier like <br>"+Reserved_Words);
