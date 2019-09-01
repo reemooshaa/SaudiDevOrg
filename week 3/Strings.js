4c28db465c98bc153b2433151ae14d5250ae0c2d
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-02 01:10:22 +0300

    Create Strings.js

---
+++ week 3/Strings.js
@@ -0,0 +1,4 @@
+var Subject="Java Script";
+document.write(Subject.length); // number of charachters 
+document.write(Subject.replace(" ","-")); // replace the letter in the first parameter to the letter in the second one.
+document.write(Subject.toUpperCase()); // Capitalize all letters in the word. 
