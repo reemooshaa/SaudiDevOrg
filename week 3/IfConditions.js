9e9987ac897a27c4212c399d81365e08cc4f898d
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-04 21:59:39 +0300

    Update IfConditions.js

--- week 3/IfConditions.js
+++ week 3/IfConditions.js
@@ -1,7 +1,7 @@
 var FirstColor="green"; SecondColor="black";
 if (FirstColor.length == SecondColor.length){ // check the length for both variables 
   document.write("Both colors have the same  length");
-  if(FirstColor.charAt(0) ==  SecondColor.charAt(0)){ // check the first letters fo both variables 
+  if(FirstColor.charAt(0) ==  SecondColor.charAt(0)){ // check the first letters for both variables 
     document.write("The first letters in both colors are the same");
   }
   else {
