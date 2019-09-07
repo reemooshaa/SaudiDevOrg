1aa2f59e6615038a56e04a44ba77aa4b5d23effc
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-07 18:38:53 +0300

    Create switch.js

---
+++ week 3/switch.js
@@ -0,0 +1,15 @@
+var studentStatus ="Junior"
+switch(studentStatus){
+  case "prepration Year":
+  document.write("The student is in prpration year");
+  break;
+  case "sophomore":
+  document.write("The student is in sophomore Year");
+  break;
+  case "Junior":
+  document.write("The student is in junior year");
+  break;
+  case "Senior":
+  document.write("The student is in Senior year");
+  break;
+}
