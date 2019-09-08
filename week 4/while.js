fa51144784e8af06fc600c7730ec49194a93ddae
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-08 22:10:17 +0300

    Create while.js

---
+++ week 4/while.js
@@ -0,0 +1,5 @@
+var evenNumber = 2; 
+while(evenNumber<=20){ //this (while) going to check all values that less than or equal to 20
+  document.write(evenNumber +'<br>');
+  evenNumber=evenNumber+2; // increment by 2 to print only Even Numbers
+}
