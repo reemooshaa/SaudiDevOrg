679ec01e593963441103cc0b1813fa10a97d5ba5
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-09 23:54:37 +0300

    Create doWhile.js

---
+++ week 4/doWhile.js
@@ -0,0 +1,5 @@
+var evenNumber = 2; 
+do{ //this (do while) going to check all values that less than or equal to 20
+  document.write(evenNumber +'<br>');
+  evenNumber=evenNumber+2; // increment by 2 to print only Even Numbers
+} while (evenNumber<=20); 
