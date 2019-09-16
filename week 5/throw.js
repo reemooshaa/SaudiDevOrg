458b9b371afa1597500ec42e6261987ad2ef0f9c
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-16 22:27:06 +0300

    Update throw.js

--- week 5/throw.js
+++ week 5/throw.js
@@ -1,7 +1,6 @@
 function evenNumber(num){
   if(num %2 == 0)
     document.write("The number is even")
-  else
-    throw ("The number is odd");
+  else throw "The number is odd";
 }
 evenNumber(3);
