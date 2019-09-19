a4d6fe477f246de90d113778d5d60e093f383a59
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-20 02:59:13 +0300

    Update TryCatchFinally.js

--- week 5/TryCatchFinally.js
+++ week 5/TryCatchFinally.js
@@ -1,10 +1,10 @@
 var num;
 function EvenNumber(num) {
- if (num%2 == 0) return x;
+ if (num%2 == 0) return num;
  else throw "The number must not be odd";
 }
 try {
- var num1 = Number(prompt("Please enter a positive integer", ""));
+ var num1 = Number(prompt("Please enter an even number", ""));
  EvenNumber(num1);
 alert(num1 + " is even");
 } catch (y) {
