9fbce962b4a9d3f7567410eb4b91255abd80d577
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-09-20 01:02:43 +0300

    Create objects.js

---
+++ week 5/objects.js
@@ -0,0 +1,11 @@
+var person = {
+  FirstName:"Ahmed",
+  LastName:"Saud",
+  Age:33,
+  Hight:190
+}
+document.write(person.Age);
+person.Age=34; // change on the object element 
+document.write(person.Age);
+person.EyeColor = "Black"; // add more element to the object 
+document.write(person.EyeColor);
