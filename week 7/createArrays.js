c9455d0ddd9340ebf38961bf40f3414740dfcea7
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-03 00:18:34 +0300

    Update createArrays.js

--- week 7/createArrays.js
+++ week 7/createArrays.js
@@ -1,4 +1,4 @@
 // Arrays 
 var languages = ["Arabic","English","Spanish"];// create array 
-var accents = new Array("Saudi","Egyption","Bahrini");// create the array by usinp Array function  
+var accents = new Array("Saudi","Egyption","Bahrini");// create the array by usinp new keyword.  
 document.write(languages,accents); //print out arrays 
