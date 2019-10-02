e420c7d327a67efa0590e1fcb1f799b5924b099b
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-03 00:51:26 +0300

    Create Exercise.js

---
+++ week 7/Exercise.js
@@ -0,0 +1,6 @@
+// This exercise aims to create two arrays one for students and the other for their GPAs and then assign each student with her GPA
+var GraduatedStudents =["Hebah","Reem","Renad","Hend"];
+var GPA = ["3.50","3.44","3.00","2.90"]
+for (var n in GraduatedStudents){
+  document.write("Student:"+ GraduatedStudents[n] +", GPA: "+ GPA[n]+ "<br>");
+}
