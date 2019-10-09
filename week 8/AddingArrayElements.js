84410314c38b3c864adcae245af9783ced5e13f0
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-09 21:30:24 +0300

    Create AddingArrayElements.js

---
+++ week 8/AddingArrayElements.js
@@ -0,0 +1,11 @@
+// there are different ways to add values to arrays. 
+var names =[];
+// First, using array name and size to assign the value. 
+names[0] = "Khaled"; 
+
+// Second, using "push" function
+names.push('Ahmed'); 
+
+// Third, assign the vlaues inside the declaration statment 
+var names =['Mohammed','Ameen'];
+document.write(names);
