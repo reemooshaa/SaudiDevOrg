30d9a3fbae13c7f62d7e395189556ac114ca13e3
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-09 22:07:25 +0300

    Create DeleteArrayElements.js

---
+++ week 8/DeleteArrayElements.js
@@ -0,0 +1,9 @@
+// ways to delete values from arrays. 
+var names =['Mohammed','Ameen'];
+// delete element number 1
+delete names[1]; 
+
+var names = ['Omar', 'Talal', 'Ahmed', 'Saad', 'Fahad']; /
+names.length=3 // reduce the size of array to 3, means we deleted last two elements. 
+names.length=0 // delete all elements 
+names.length=5 // recover the size but the array is empty 
