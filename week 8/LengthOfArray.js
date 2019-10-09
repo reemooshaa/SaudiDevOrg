c4c0e8380989c44d394a8b174e94a749cd393d6e
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-09 21:16:44 +0300

    Create LengthOfArray.js

---
+++ week 8/LengthOfArray.js
@@ -0,0 +1,5 @@
+var names =[]; // create an array with no values and no size 
+var names = new Array(2) //specify an array with size =2
+names.length =5; // extend the array length to 5 
+var names = ['Ahmed','Khaled','Mohammed'];
+document.write(names);
