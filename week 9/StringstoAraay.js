8de7aaf52b83684f37b354072706a5c1489743c9
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-20 01:10:04 +0300

    Create StringstoAraay.js

---
+++ week 9/StringstoAraay.js
@@ -0,0 +1,12 @@
+var str = "HELLO WORLD";
+var res = str.charAt(0); 
+var resEnd = str.charAt(str.length-1);
+document.write(res);
+document.write("<br>");
+document.write(resEnd);
+
+// check if there is a word, it returns true 
+var str = "Hello world, welcome to the universe.";
+var n = str.includes("world");
+document.write(n);
+
