be4b81a2e6a853225b2ca6a1848eaadf76ba53a2
Reem <48737787+reemooshaa@users.noreply.github.com>
2019-10-20 00:17:11 +0300

    Create ArrayMethods.js

---
+++ week 9/ArrayMethods.js
@@ -0,0 +1,19 @@
+ var fruits = ["Banana", "Orange", "Apple", "Mango"] ;
+ // using join() with array will generate the elements of it as a string
+ document.write(fruits.join());
+ 
+ // reverse the order of the array 
+ document.write(fruits.reverse());
+
+// sort the array
+ document.write(fruits.sort());
+ 
+ // concatenate different arrays
+ var fruits1 = ["Banana", "Orange"];
+ var fruits2 = ["Apple", "Mango"];
+ document.write(fruits1.concat(fruits2));
+ 
+ // seprate the array, the first parameter indicate the first index that we should cut the array from and the other parameter is the end.
+  Var fruits = [“Banana”, “Orange”, “Lemon”, “Apple”, “Mango”];
+ document.write(fruits.slice(1, 3));
+ 
